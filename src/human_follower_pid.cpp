9856bb3cdf1b1385640799cc5e32111f023aa06c
Justin10612 <atom.9031@gmail.com>
2023-12-01 05:41:53 +0800

    Tuning PID

--- src/human_follower_pid.cpp
+++ src/human_follower_pid.cpp
@@ -9,6 +9,7 @@
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "geometry_msgs/msg/vector3.hpp"
+#include "std_msgs/msg/float64.hpp"
 
 
 using namespace std::chrono_literals;
@@ -24,7 +25,7 @@ const double MAX_LINEAR_VEL_OUTPUT = 1.5;
 /* Angle constant */
 const double MAX_ANGULER_VEL_OUTPUT = 2; 
 /* Depth pid controller */
-const double DEPTH_kp = 1.6;
+const double DEPTH_kp = 1.2;
 const double DEPTH_kd = 1.2;
 double depth_error1 = 0;
 /* Angle pid controller */
@@ -36,6 +37,9 @@ double target_angle = 0.0;
 double target_depth = 0.0;
 double target_state = 0.0;
 bool follow_flag = false;
+
+double depth_error=0.0;
+double angle_error=0.0; 
 
 double PD_Controller(double error, double error1, double max, double kp, double kd);
 
@@ -51,6 +55,8 @@ class HumanFollowerPID : public rclcpp::Node
             "robot_mode", 10, std::bind(&HumanFollowerPID::mode_callback, this, std::placeholders::_1));
         human_pose_sub_ = this->create_subscription<geometry_msgs::msg::Vector3>(
             "human_pose", 10, std::bind(&HumanFollowerPID::human_pose_callback, this, std::placeholders::_1));
+        depth_sub_ = this->create_subscription<std_msgs::msg::Float64>(
+            "depth", 10, std::bind(&HumanFollowerPID::depth_callback, this, std::placeholders::_1));
     }
 
   private:
@@ -59,6 +65,7 @@ class HumanFollowerPID : public rclcpp::Node
     // Subscriber
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr robot_mode_sub;
     rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr human_pose_sub_;
+    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr depth_sub_;
     
 
     void mode_callback(const std_msgs::msg::String::SharedPtr mode_msgs) const
@@ -67,35 +74,42 @@ class HumanFollowerPID : public rclcpp::Node
         std::string mode = mode_msgs->data;
         // Message 
         auto cmd_vel_msgs = geometry_msgs::msg::Twist();
-
         /* Follow mode */
-        if(mode=="FOLLOW" && target_state==1.0){
+        if(mode=="FOLLOW"){
             follow_flag = true;
             // RCLCPP_INFO(this->get_logger(), "Following");
-            /* Error cal */
-            double depth_error = target_depth - MIN_CHASE_DISTANCE;
-            double angle_error = 0.0-target_angle;
-            if(fabs(angle_error)<0.3) angle_error=0;
-            /* ZONE Detect */
-            if(target_depth > MAX_CHASE_DISTANCE){
-                /* STOP ZONE */
-                cmd_vel_msgs.linear.x = 0.0;
-                cmd_vel_msgs.angular.z = 0.0;
+            if(target_state == 1.0){
+                /* Error cal */
+                depth_error = target_depth - MIN_CHASE_DISTANCE;
+                angle_error = 0.0-target_angle;
+                if(fabs(angle_error)<0.3) angle_error=0;
+                /* ZONE Detect */
+                if(target_depth > MAX_CHASE_DISTANCE){
+                    /* STOP ZONE */
+                    cmd_vel_msgs.linear.x = 0.0;
+                    cmd_vel_msgs.angular.z = 0.0;
+                }else{
+                    /* CHASING ZONE */
+                    cmd_vel_msgs.linear.x = PD_Controller(
+                        depth_error, depth_error1, MAX_LINEAR_VEL_OUTPUT, DEPTH_kp, DEPTH_kd
+                    );
+                    // RCLCPP_INFO(this->get_logger(), "Output = %f", depth_error);
+                    cmd_vel_msgs.angular.z = PD_Controller(
+                        angle_error, angle_error1, MAX_ANGULER_VEL_OUTPUT, ANGLE_kp, ANGLE_kd
+                    );
+                }   
+                /* LAST ERROR */
+                depth_error1 = depth_error;
+                angle_error1 = angle_error;
+                follow_vel_pub_->publish(cmd_vel_msgs);
             }else{
-                /* CHASING ZONE */
-                cmd_vel_msgs.linear.x = PD_Controller(
-                    depth_error, depth_error1, MAX_LINEAR_VEL_OUTPUT, DEPTH_kp, DEPTH_kd
-                );
-                cmd_vel_msgs.angular.z = PD_Controller(
-                    angle_error, angle_error1, MAX_ANGULER_VEL_OUTPUT, ANGLE_kp, ANGLE_kd
-                );
+                depth_error=0;
+                angle_error=0;  
             }
-            /* LAST ERROR */
-            depth_error1 = depth_error;
-            angle_error1 = angle_error;
-            follow_vel_pub_->publish(cmd_vel_msgs);
         }else{
             // RCLCPP_INFO(this->get_logger(), "Lost target, zero output");
+            depth_error=0;
+            angle_error=0;
             // Clean Output
             cmd_vel_msgs.linear.x = 0.0;
             cmd_vel_msgs.angular.z = 0.0;
@@ -111,8 +125,13 @@ class HumanFollowerPID : public rclcpp::Node
     {
         double target_x = target_msgs->x;
         target_angle = (target_x-640)*0.001225; // Change pixel to radian 0.001225 = (45/640)*(pi/180)
-        target_depth = (target_msgs->y);
+        // target_depth = (target_msgs->y);
         target_state = target_msgs->z;
+    }
+
+    void depth_callback(const std_msgs::msg::Float64::SharedPtr depth_msg) const
+    {
+        target_depth = float(depth_msg->data);
     }
 };
 
